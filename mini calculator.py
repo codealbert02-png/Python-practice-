a365f8ca413cd54f2b15c2d7dc355493d79e4230
codealbert02-png <codealbert02@gmail.com>
2026-03-23 14:38:51 +0600

    Update mini calculator.py

--- mini calculator.py
+++ mini calculator.py
@@ -1,5 +1,10 @@
-def add(a,b):
-    return a + b
-
-    result = add(5+3)
-    print(result)
+import random
+randomnumber=random.randrange(1,200)
+print(randomnumber)
+userinput=int(input("guess the number:"))
+if userinput > randomnumber:
+    print("the number is to high")
+elif randomnumber>userinput:
+        print("the number is too low")
+else:
+        print("yes,you matched the number")
